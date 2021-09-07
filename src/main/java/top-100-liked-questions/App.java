536dced0abe94a69696e4786d22f5d72ee888c41
“wsl” <1251596018@qq.com>
2021-09-07 10:37:44 +0800

    init the leetcode repo

---
+++ src/main/java/top-100-liked-questions/App.java
@@ -0,0 +1,13 @@
+package top-100-liked-questions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
